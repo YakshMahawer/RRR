4bfbd5a5f5eecca19d51a7d86a516f531e2ddfc2
Renji <aadarshjha1401@gmail.com>
2023-01-25 23:39:12 +0530

    admin and verify page up and running

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,39 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const app = express();
+const connectDB = require('./db/conn')
+require('dotenv').config()
+const bcrypt = require('bcryptjs')
+const AdminData = require('./models/adminDataSchema')
+const OtpData = require('./models/otpSchema')
+
+const routing = require('./routes/tasks')
+
+// Schemas
 const Complaint = require("./models/complaintschema");
 const Voterinfo = require("./models/voterinfoschema");
 const Area = require("./models/areaschema");
-const app = express();
-require("./conn.js");
-app.listen(7070, ()=>{
-    console.log("Serverr Done");
-});
+
+// Middlewares
+app.use(express.json())
+
+
+// Routing
+app.use('/', routing)
+
+
+
+// Starting server
+const port = 7070
+const start = async()=>{
+    try {
+        await connectDB(process.env.MONGO_URI).then(()=>{
+            console.log('Database connection establilshed');
+            app.listen(port, ()=>{
+                console.log(`Server is listening on port ${port}...`);
+            })
+        })
+    } catch (error) {
+        console.log('Database not connecting');
+    }
+}
+start()
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
+import React, { useState } from 'react';
 import { Route, Routes } from "react-router-dom";
 import Complain from "./pages/Complain/Complain";
 import Statistics from "./pages/Statistics/Statistics";
 import Status from "./pages/Status/Status";
 import Thanks from "./pages/Thanks/Thanks";
 import AdminLogin from "./Components/AdminLogin";
+import OTP from './Components/OTP';
 
 function App() {
+
+  const [urlMail, setUrlMail] = useState('')
+  const [verified, setVerified] = useState(false)
+
   return (
     <div className="App">
       <Routes>
@@ -13,7 +19,8 @@ function App() {
         <Route exact path="/statistics" element={<Statistics />} />
         <Route exact path="/status" element={<Status />} />
         <Route exact path="/thanks" element={<Thanks />} />
-        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/admin" element={<AdminLogin setUrlMail={setUrlMail} verified={verified}/>} />
+        <Route path='/verify/*' element={<OTP urlMail={urlMail} setVerified={setVerified}/>} />
       </Routes>
     </div>
   );
