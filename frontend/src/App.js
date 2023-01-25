f28b123097913dad9729c2a48722126a641964cb
Renji <aadarshjha1401@gmail.com>
2023-01-25 12:16:21 +0530

    Basic AdminLogin Page Created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import AdminLogin from './Components/AdminLogin';
+
+
+/** routes */
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <div>Home page</div>
+  },
+
+  {
+    path:'/admin',
+    element: <AdminLogin />
+  },
+
+])
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <RouterProvider router={router} />
+    </main>
   );
 }
 
