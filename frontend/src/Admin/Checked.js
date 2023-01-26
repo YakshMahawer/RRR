5533d73472438d6c8744589e3eb1e89c0d2abe82
Rohit Singh <rohitsingh66604@gmail.com>
2023-01-26 19:42:28 +0530

    Admin Components

---
+++ frontend/src/Admin/Checked.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import Sidebar from './Sidebar'
+
+const Checked = () => {
+  return (
+    <Fragment>
+      <Sidebar />
+      <div>Checked Complaints</div>
+    </Fragment>
+  )
+}
+
+export default Checked
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,11 @@ import Complain from "./pages/Complain/Complain";
 import Statistics from "./pages/Statistics/Statistics";
 import Status from "./pages/Status/Status";
 import Thanks from "./pages/Thanks/Thanks";
-import AdminLogin from "./Components/AdminLogin";
+import AdminLogin from "./Admin/AdminLogin";
+import AdminHome from "./Admin/AdminHome";
+import MyComplaints from "./Admin/MyComplaints";
+import Checked from "./Admin/Checked";
+import AllComplaints from "./Admin/AllComplaints";
 
 function App() {
   return (
@@ -13,7 +17,11 @@ function App() {
         <Route exact path="/statistics" element={<Statistics />} />
         <Route exact path="/status" element={<Status />} />
         <Route exact path="/thanks" element={<Thanks />} />
-        <Route path="/admin" element={<AdminLogin />} />
+        <Route exact path="/admin" element={<AdminLogin />} />
+        <Route exact path="/admin/home" element={<AdminHome />} />
+        <Route exact path="/admin/myComplaints" element={<MyComplaints />} />
+        <Route exact path="/admin/checkedComplaints" element={<Checked />} />
+        <Route exact path="/admin/allComplaints" element={<AllComplaints />} />
       </Routes>
     </div>
   );
