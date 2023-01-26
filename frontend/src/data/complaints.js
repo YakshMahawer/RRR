2af3f00e00660022b144b493f2d0172b9fa891c8
himanshu <hmxnsu.blvck@gmail.com>
2023-01-27 02:50:15 +0530

    _added_state_management_and_statistics_component_

---
+++ frontend/src/data/complaints.js
@@ -0,0 +1,27 @@
+import area from "./data"
+
+const complaints = [
+    {
+        id: 1,
+        voter_Id: 1,
+        complaint: "I am not able to vote",
+        status: "pending",
+        area: area[0].name,
+    },
+    {
+        id: 2,
+        voter_Id: 2,
+        complaint: "I sssssssssssssable to vote",
+        status: "pending",
+        area: area[2].name,
+    },
+    {
+        id: 3,
+        voter_Id: 3,
+        complaint: "I am not able to vote",
+        status: "pending",
+        area: area[2].name,
+    },
+]
+
+export default complaints
